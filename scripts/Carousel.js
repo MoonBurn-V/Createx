bd6ff339d49fc0dff26af08946f6f5dc0c9499c1
Valery Redkin <valeriy.redkin.05@mail.ru>
2025-07-14 21:22:44 +0300

    Editing home page

--- scripts/Carousel.js
+++ scripts/Carousel.js
@@ -22,15 +22,42 @@ class CarouselInstance {
         this.container = container;
         this.index = index;
         this.carousel = carousel;
+        this.cardContainerElement = document.querySelector(`[data-js-card-container][index="${index}"]`);
         this.cardElement = this.container.querySelector('[data-js-card]');
         this.btnPrevElement = document.querySelector(`[data-js-btn-prev][index="${index}"]`);
         this.btnNxtElement = document.querySelector(`[data-js-btn-nxt][index="${index}"]`);
         this.padding = (index === 1) ? 105 : 0;
         this.cardWidth = this.cardElement ? this.cardElement.getBoundingClientRect().width : 0;
+        this.touchStartY = 0;
+        this.touchStartX = 0;
 
         this.bindEvents();
 
         if (this.index === 1) {
+            this.cardContainerElement.addEventListener('wheel', (event) => {
+                if(!this.isVerticalScroll(event)){
+                    event.preventDefault();
+                }
+            })
+            
+            this.cardContainerElement.addEventListener('touchstart', (event) => {
+                this.touchStartY = event.touches[0].clientY;
+                this.touchStartX = event.touches[0].clientX;
+            })
+            
+            this.cardContainerElement.addEventListener('touchmove', (event) => {
+                this.touchEndY = event.touches[0].clientY;
+                this.touchEndX = event.touches[0].clientX;
+
+                this.deltaY = this.touchEndY - this.touchStartY;
+                this.deltaX = this.touchEndX - this.touchStartX;
+
+                if(Math.abs(this.deltaY) <= Math.abs(this.deltaX)) {
+                    event.preventDefault();
+                }
+            })
+
+
             this.chengeLine = new ChengeLine(this.index, this.btnPrevElement, this.btnNxtElement);
         } else {
             this.chengeLine = null;
@@ -58,7 +85,118 @@ class CarouselInstance {
             this.container.scrollLeft += this.cardWidth + this.padding;
         }
     }
+
+    isVerticalScroll = (event) => {
+        // Проверяем, больше ли абсолютное значение изменения по оси Y, чем по оси X
+        return Math.abs(event.deltaY) > Math.abs(event.deltaX);
+    }
 }
+
+
+// На случай если прокрутка появится
+// class Carousel {
+//     selectors = {
+//         cardContainer: '[data-js-card-container]',
+//         card: '[data-js-card]',
+//         btnPrev: '[data-js-btn-prev]',
+//         btnNxt: '[data-js-btn-nxt]',
+//         lines: '[data-js-lines]',
+//     }
+
+//     constructor() {
+//         this.carousels = [];
+
+//         const cardContainerElements = document.querySelectorAll(this.selectors.cardContainer);
+//         cardContainerElements.forEach((container, index) => {
+//             this.carousels.push(new CarouselInstance(container, index, this));
+//         });
+//     }
+// }
+
+// class CarouselInstance {
+//     constructor(container, index, carousel) {
+//         this.container = container;
+//         this.index = index;
+//         this.carousel = carousel;
+//         this.cardContainerElement = document.querySelector(`[data-js-card-container][index="${index}"]`);
+//         this.cardElement = this.container.querySelector('[data-js-card]');
+//         this.btnPrevElement = document.querySelector(`[data-js-btn-prev][index="${index}"]`);
+//         this.btnNxtElement = document.querySelector(`[data-js-btn-nxt][index="${index}"]`);
+//         this.padding = (index === 1) ? 105 : 0;
+//         this.cardWidth = this.cardElement ? this.cardElement.getBoundingClientRect().width : 0;
+//         this.touchStartY = 0;
+//         this.touchStartX = 0;
+//         this.touchEndTime = 0;
+
+//         this.bindEvents();
+
+//         if (this.index === 1) {
+//             this.cardContainerElement.addEventListener('wheel', (event) => {
+//                 if(!this.isVerticalScroll(event)){
+//                     event.preventDefault();
+//                 }
+//             })
+            
+//             this.cardContainerElement.addEventListener('touchstart', (event) => {
+//                 this.touchStartY = event.touches[0].clientY;
+//                 this.touchStartX = event.touches[0].clientX;
+//                 this.touchEndTime = new Date().getTime();
+//             })
+            
+//             this.cardContainerElement.addEventListener('touchmove', (event) => {
+//                 this.currentTime = new Data().getTime();
+
+//                 if(this.currentTime - this.touchEndTime > 200) {
+//                     return;
+//                 }
+
+//                 this.touchEndY = event.touches[0].clientY;
+//                 this.touchEndX = event.touches[0].clientX;
+
+//                 this.deltaY = this.touchEndY - this.touchStartY;
+//                 this.deltaX = this.touchEndX - this.touchStartX;
+
+//                 this.horizontalThreshold = 20;
+
+//                 if(Math.abs(this.deltaY) > Math.abs(this.deltaX) + this.horizontalThreshold) {
+//                     event.preventDefault();
+//                 }
+//             })
+
+
+//             this.chengeLine = new ChengeLine(this.index, this.btnPrevElement, this.btnNxtElement);
+//         } else {
+//             this.chengeLine = null;
+//         }
+//     }
+
+//     bindEvents() {
+//         if (this.btnPrevElement) {
+//             this.btnPrevElement.addEventListener('click', this.scrollPrev);
+//         }
+
+//         if (this.btnNxtElement) {
+//             this.btnNxtElement.addEventListener('click', this.scrollNxt);
+//         }
+//     }
+
+//     scrollPrev = () => {
+//         if (this.cardElement) {
+//             this.container.scrollLeft -= this.cardWidth + this.padding + 5;
+//         }
+//     }
+
+//     scrollNxt = () => {
+//         if (this.cardElement) {
+//             this.container.scrollLeft += this.cardWidth + this.padding;
+//         }
+//     }
+
+//     isVerticalScroll = (event) => {
+//         // Проверяем, больше ли абсолютное значение изменения по оси Y, чем по оси X
+//         return Math.abs(event.deltaY) > Math.abs(event.deltaX);
+//     }
+// }
 
 class ChengeLine {
     selectors = {
