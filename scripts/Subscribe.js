fd001720865ac69d7690e8ec4d4fa4acb4f3fc4b
Valery Redkin <valeriy.redkin.05@mail.ru>
2025-07-16 21:15:40 +0300

    Subscribe1

--- scripts/Subscribe.js
+++ scripts/Subscribe.js
@@ -1,4 +1,4 @@
-class Subscribe{
+class Subscribe {
     selectors = {
         subscribeHeaders: '[data-js-subscribe-headers]',
         footerHeaders: '[data-js-footer-headers]',
@@ -17,37 +17,54 @@ class Subscribe{
     constructor() {
         this.subscribeHeadersElements = document.querySelectorAll(this.selectors.subscribeHeaders + ' .subscribe__title');
         this.footerHeadersElements = document.querySelectorAll(this.selectors.footerHeaders + ' .footer__menu-column-title');
-        this.emailInputElement = document.querySelector(this.selectors.emailInput);
-        this.errorMassegeElement = document.querySelector(this.selectors.errorMassege);
-        this.btnSubmitElement = document.querySelector(this.selectors.btnSubmit);
+        this.recommendationElement = document.querySelector(this.selectors.recommendation);
+        this.btnSubmitElements = document.querySelectorAll(this.selectors.btnSubmit);
         this.bindEvents();
 
-        if(this.subscribeHeadersElements.length > 0) {
+        if (this.subscribeHeadersElements.length > 0) {
             this.subscribeHeadersElements[0].classList.add(this.stateClasses.visible)
         }
     }
 
     bindEvents() {
-        if(this.btnSubmitElement) {
-            this.btnSubmitElement.addEventListener('click', this.checkEmailInput)
-        }
+        this.btnSubmitElements.forEach(button => {
+            button.addEventListener('click', this.checkEmailInput);
+        });
     }
 
     checkEmailInput = (event) => {
         event.preventDefault();
 
-        this.emailContent = this.emailInputElement ? this.emailInputElement.value : '';
-        this.emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        const button = event.target;
+        const index = parseInt(button.getAttribute('index'));
 
-        if(!this.emailRegex.test(this.emailContent)) {
-            this.emailInputElement.classList.add(this.stateClasses.activeError);
-            this.errorMassegeElement.classList.add(this.stateClasses.activeError);
-        }else if (this.emailRegex.test(this.emailContent)) {
-            this.emailInputElement.classList.remove(this.stateClasses.activeError);
-            this.errorMassegeElement.classList.remove(this.stateClasses.activeError);
-            this.emailInputElement.classList.add(this.stateClasses.done);
-            this.subscribeHeadersElements[0].classList.remove(this.stateClasses.visible);
-            this.subscribeHeadersElements[1].classList.add(this.stateClasses.visible);
+        const emailInput = document.querySelector(`${this.selectors.emailInput}[index="${index}"]`);
+        const errorMessage = document.querySelector(`${this.selectors.errorMassege}[index="${index}"]`);
+
+        const emailContent = emailInput ? emailInput.value : '';
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+        if (!emailRegex.test(emailContent)) {
+            emailInput.classList.add(this.stateClasses.activeError);
+            errorMessage.classList.add(this.stateClasses.activeError);
+        } else {
+            emailInput.classList.remove(this.stateClasses.activeError);
+            errorMessage.classList.remove(this.stateClasses.activeError);
+            emailInput.classList.add(this.stateClasses.done);
+
+            if (index === 0) {
+                if (this.subscribeHeadersElements.length > 1) {
+                    this.subscribeHeadersElements[0].classList.remove(this.stateClasses.visible);
+                    this.subscribeHeadersElements[1].classList.add(this.stateClasses.visible);
+                }
+            }
+            else if (index === 1) {
+                if (this.footerHeadersElements.length > 1) {
+                    this.footerHeadersElements[0].classList.remove(this.stateClasses.visible);
+                    this.footerHeadersElements[1].classList.add(this.stateClasses.visible);
+                    this.recommendationElement.classList.remove(this.stateClasses.visible);
+                }
+            }
         }
     }
 }
